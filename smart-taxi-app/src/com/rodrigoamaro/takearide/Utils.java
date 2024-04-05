b818a89fabee85287cc721e2f3917545c93b6005
Rodrigo Amaro <rodrigo.amaro.r@gmail.com>
2024-04-05 10:30:36 -0300

    Update Utils.java

--- smart-taxi-app/src/com/rodrigoamaro/takearide/Utils.java
+++ smart-taxi-app/src/com/rodrigoamaro/takearide/Utils.java
@@ -10,6 +10,7 @@ public class Utils {
     static final void LogError(Context ctx, String tag, Exception e) {
         String txt = ctx.getClass().toString() + " " + e.toString() + " " + e.getMessage();
         Log.e(tag, txt);
+        Log.e("test", "lets get the toast roar!");
         Toast.makeText(ctx, txt, Toast.LENGTH_LONG).show();
     }
 }
